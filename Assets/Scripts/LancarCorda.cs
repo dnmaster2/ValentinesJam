152c533c12e3459dd759e3913f3c0ff67a75c004
Nathan Morsch <54150372+INotCamper@users.noreply.github.com>
2020-02-14 13:15:41 -0300

    Corda Com joint inutil (por enquanto)

---
+++ Assets/Scripts/LancarCorda.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LancarCorda : MonoBehaviour
+{
+    public GameObject Gancho;
+    GameObject atualGancho;
+    public bool cordaAtiva = false;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!cordaAtiva)
+            {
+                Vector2 destino = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                atualGancho = Instantiate(Gancho, transform.position, Quaternion.identity);
+                atualGancho.GetComponent<Corda>().destino = destino;
+                cordaAtiva = true;
+            }
+            else
+            {
+                Destroy(atualGancho);
+                cordaAtiva = false;
+            }
+        }        
+    }
+}
