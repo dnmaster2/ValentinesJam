21734e99473cd10ba77d976b63f702302e8f1922
Nathan Morsch <54150372+INotCamper@users.noreply.github.com>
2020-02-14 14:56:10 -0300

    Concorda

--- Assets/Scripts/CordaDistance.cs
+++ Assets/Scripts/CordaDistance.cs
@@ -14,41 +14,53 @@ public class CordaDistance : MonoBehaviour
     bool acabou = false;
     public List<GameObject> Nodes = new List<GameObject>();
     LineRenderer lr;
+    public GameObject player2;
+    bool grudada = false;
+    public float minDistanciaCorda = .25f, maxDistanciaCorda = 5f;
     #endregion
     void Start()
     {
         lr = GetComponent<LineRenderer>();
         player = GameObject.FindGameObjectWithTag("Player");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
         ultimoNode = transform.gameObject;
         Nodes.Add(transform.gameObject);
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, destino, Velocidade);
-        if ((Vector2)transform.position != destino)
+        if (grudada)
         {
-            if (Vector2.Distance(player.transform.position, ultimoNode.transform.position) >= distancia)
+            transform.position = player2.transform.position;
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destino, Velocidade);
+            if ((Vector2)transform.position != destino)
             {
-                CriarNodo();
+                if (Vector2.Distance(player.transform.position, ultimoNode.transform.position) >= distancia)
+                {
+                    CriarNodo();
+                }
+            }
+            else if (!acabou)
+            {
+                acabou = true;
+                while (Vector2.Distance(player.transform.position, ultimoNode.transform.position) >= distancia)
+                {
+                    CriarNodo();
+                }
+                ultimoNode.GetComponent<DistanceJoint2D>().connectedBody = player.GetComponent<Rigidbody2D>();
             }
         }
-        else if (!acabou)
-        {
-            acabou = true;
-            while (Vector2.Distance(player.transform.position, ultimoNode.transform.position) >= distancia)
-            {
-                CriarNodo();
-            }
-            ultimoNode.GetComponent<DistanceJoint2D>().connectedBody = player.GetComponent<Rigidbody2D>();
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        if (Input.GetAxis("Mouse ScrollWheel") != 0 && (Nodes[0].GetComponent<DistanceJoint2D>().distance > minDistanciaCorda && Nodes[0].GetComponent<DistanceJoint2D>().distance < maxDistanciaCorda))
         {
             for (int i = 0; i < Nodes.Count; i++)
             {
                 Nodes[i].GetComponent<DistanceJoint2D>().distance -= Input.GetAxis("Mouse ScrollWheel") * 10 * Time.deltaTime;
             }
         }
+        
         RenderizarLinha();
     }
     void CriarNodo()
@@ -60,6 +72,7 @@ public class CordaDistance : MonoBehaviour
         GameObject go = Instantiate(PrefabNode, posCriar, Quaternion.identity);
         go.transform.SetParent(transform);
         ultimoNode.GetComponent<DistanceJoint2D>().connectedBody = go.GetComponent<Rigidbody2D>();
+        ultimoNode.GetComponent<DistanceJoint2D>().distance = distancia;
         ultimoNode = go;
         Nodes.Add(ultimoNode);
     }
@@ -72,5 +85,13 @@ public class CordaDistance : MonoBehaviour
             lr.SetPosition(i, Nodes[i].transform.position);
         }
         lr.SetPosition(i, player.transform.position);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player2")
+        {
+            grudada = true;
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
     }
 }
