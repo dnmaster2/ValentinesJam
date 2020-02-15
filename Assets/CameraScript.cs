1a506a544344ec20325c49d02eff48860a68b547
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-02-15 19:58:11 -0300

    Camera dinamica

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraScript : MonoBehaviour
 {
     public List<Transform> targets;
-    public Vector3 ajuste;
+    public Vector3 ajuste = new Vector3(0,0,-10);
+    public float zoomMinimo = 3f, zoomMaximo = 10f, limiteZoom = 18f;
     Vector3 velocity;
-    float smoothTime = .5f;
+    float smoothTime = .2f;
+    Camera cam;
 
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
     void LateUpdate()
     {
         transform.position = Vector3.SmoothDamp(transform.position, GetCenterPoint() + ajuste, ref velocity, smoothTime);
-
+        float actualZoom = Mathf.Lerp(zoomMinimo, zoomMaximo, GetDistancePoint() / limiteZoom);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, actualZoom, Time.deltaTime);
     }
 
     Vector3 GetCenterPoint()
@@ -23,5 +31,15 @@ public class CameraScript : MonoBehaviour
             bound.Encapsulate(target.position);
         }
         return bound.center;
+    }
+
+    float GetDistancePoint()
+    {
+        Bounds bound = new Bounds(targets[0].position, Vector2.zero);
+        foreach (Transform target in targets)
+        {
+            bound.Encapsulate(target.position);
+        }
+        return bound.size.x;
     }
 }
