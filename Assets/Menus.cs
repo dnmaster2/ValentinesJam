bff34a5aa316a2c5e2aba4560efbbce240f81959
Nathan Morsch <54150372+INotCamper@users.noreply.github.com>
2020-02-15 22:23:45 -0300

    Adicionado som aos botões, e arrumado o lugar da morte

--- Assets/Menus.cs
+++ Assets/Menus.cs
@@ -5,17 +5,25 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour
-{    
-    public void MainMenu()
+{
+    AudioSource som;
+    private void Start()
     {
-        SceneManager.LoadScene(0);
+        som = GetComponent<AudioSource>();
     }
     public void StartButton()
     {
+        AudioClick();
         SceneManager.LoadScene(1);
     }
     public void QuitButton()
     {
+        AudioClick();
         Application.Quit();
+    }
+    void AudioClick()
+    {
+        som.pitch = Random.Range(.5f, 1.5f);
+        som.Play();
     }
 }
--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -7,6 +7,11 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject PauseMenuUI;
     public GameObject BotaoPause;
+    AudioSource som;
+    private void Start()
+    {
+        som = GetComponent<AudioSource>();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -18,17 +23,26 @@ public class PauseMenu : MonoBehaviour
     {
         PauseMenuUI.SetActive(!PauseMenuUI.activeSelf);
         BotaoPause.SetActive(!BotaoPause.activeSelf);
+        AudioClick();
     }
     public void Restart()
     {
+        AudioClick();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void MainMenu()
     {
+        AudioClick();
         SceneManager.LoadScene(0);
     }
     public void QuitDesktop()
     {
+        AudioClick();
         Application.Quit();
+    }
+    void AudioClick()
+    {
+        som.pitch = Random.Range(.5f, 1.5f);
+        som.Play();
     }
 }
