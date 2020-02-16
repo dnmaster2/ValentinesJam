ff5aec5e0dad9a3eee15d6108c719987d6e33b99
Nathan Morsch <54150372+INotCamper@users.noreply.github.com>
2020-02-16 01:21:49 -0300

    Cometei mais uns ai

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -6,24 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour
 {
+    //som do click do botao sai desse lugar
     AudioSource som;
     private void Start()
     {
+        //preenche o lugar onde esta co click do botao
         som = GetComponent<AudioSource>();
     }
     public void StartButton()
     {
+        //executa a funcao de som
         AudioClick();
+        //fecha o jogo
         SceneManager.LoadScene(1);
     }
     public void QuitButton()
     {
+        //executa a funcao de som
         AudioClick();
+        //fecha o jogo
         Application.Quit();
     }
     void AudioClick()
     {
+        //randomiza o pitch do click para parecer clicks diferentes, mas é sempre o mesmo
         som.pitch = Random.Range(.5f, 1.5f);
+        //da play no som
         som.Play();
     }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,44 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    //menu de pausa
     public GameObject PauseMenuUI;
+    //botao de pausa
     public GameObject BotaoPause;
+    //som do click do botao sai desse lugar
     AudioSource som;
     private void Start()
     {
+        //preenche o lugar onde esta co click do botao
         som = GetComponent<AudioSource>();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            //executa a funcao de pause
             PauseAndResume();
         }
     }
     public void PauseAndResume()
     {
+        //inverte o estado do menu de pausa
         PauseMenuUI.SetActive(!PauseMenuUI.activeSelf);
+        //inverte o estado do botao de pausa
         BotaoPause.SetActive(!BotaoPause.activeSelf);
+        //executa a funcao de som
         AudioClick();
     }
     public void Restart()
     {
+        //executa a funcao de som
         AudioClick();
+        //carrega a cena que estava
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void MainMenu()
     {
+        //executa a funcao de som
         AudioClick();
+        //carrega a cena 0 (main menu)
         SceneManager.LoadScene(0);
     }
     public void QuitDesktop()
     {
+        //executa a funcao de som
         AudioClick();
+        //fecha o jogo
         Application.Quit();
     }
     void AudioClick()
     {
+        //randomiza o pitch do click para parecer clicks diferentes, mas é sempre o mesmo
         som.pitch = Random.Range(.5f, 1.5f);
+        //da play no som
         som.Play();
     }
 }
