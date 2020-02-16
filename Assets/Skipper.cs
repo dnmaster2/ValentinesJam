6616154c9d8901324dc76c4f9aa89323cf3f2a11
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-02-16 18:39:39 -0300

    um monte de coisa

---
+++ Assets/Skipper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skipper : MonoBehaviour
+{
+    public GameObject fadeout, canvasRef;
+    public AnimationClip anim;
+    void Start()
+    {
+        StartCoroutine(PulaCena());
+    }
+
+    IEnumerator PulaCena()
+    {
+        yield return new WaitForSeconds(anim.length);
+        Instantiate(fadeout, canvasRef.transform);
+    }
+}
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    public List<AudioClip> clips;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
